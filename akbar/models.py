5ebb6dd6a35b67352d0a4eb983703add35279942
Ritik <ritik.2201051cs@iiitbh.ac.in>
2024-10-18 11:16:08 +0530

    bc

--- akbar/models.py
+++ akbar/models.py
@@ -1,9 +1,19 @@
 from django.db import models
-from django.utils import timezone
+# from django.utils import timezone
+
+# class DumpStats(models.Model):
+#     loc = models.CharField(max_length=100)
+    
+    
+#     def __str__(self):
+#         return self.loc
+
 
 class DumpStats(models.Model):
-    loc = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    latitude = models.FloatField(default=0.0)
+    longitude = models.FloatField(default=0.0)
     full = models.BooleanField()
-    
+
     def __str__(self):
-        return self.loc
+        return self.name
--- akbar/views.py
+++ akbar/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, redirect
-
+from .models import DumpStats
 
 def home(request):
     return render(request, 'akbar/home.html')
 
 def location(request):
     return render(request, 'akbar/location.html')
+
+
+def map_view(request):
+    locations = DumpStats.objects.all()
+    return render(request, 'akbar/map.html', {'locations': locations})
